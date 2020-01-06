ea6d228e0a93317259a3e54c27293a4a91626d1c
Alan <43737208+alan-w25@users.noreply.github.com>
2020-01-05 23:18:48 -0500

    copied code

--- src/main/kotlin/TrajectoryGen.kt
+++ src/main/kotlin/TrajectoryGen.kt
@@ -2,6 +2,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.acmerobotics.roadrunner.path.heading.ConstantInterpolator
 import com.acmerobotics.roadrunner.path.heading.HeadingInterpolator
+import com.acmerobotics.roadrunner.path.heading.LinearInterpolator
 import com.acmerobotics.roadrunner.path.heading.SplineInterpolator
 import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
@@ -14,24 +15,54 @@ object TrajectoryGen {
     fun createTrajectory(): ArrayList<Trajectory> {
         val list = ArrayList<Trajectory>()
 
-        val builder1 = TrajectoryBuilder(Pose2d(), constraints)
-
-        // dump routine
-        /*builder
-            .setReversed(true)
-            .splineTo(Pose2d(-12.0, -42.0, 180.0.toRadians))// y = -39 is halfway between the skybridge and partner with 6" on either side, y = -42 gives 3" of room for other robot
-            .splineTo(Pose2d(28.0, -42.0, 180.0.toRadians))
-            .splineTo(Pose2d(48.0, -33.0, -90.0.toRadians))
-            .lineTo(Vector2d(48.0, -26.0), ConstantInterpolator(-90.0.toRadians))*/
+        val builder1 = TrajectoryBuilder(Pose2d(-33.0,63.0, 180.0.toRadians), constraints)
 
 
-        // weird reversed profiles
-        /* builder
-             .setReversed(true)
-             .splineTo(Pose2d(-12.0, -42.0, 180.0.toRadians), SplineInterpolator(180.0.toRadians, startPose.heading))// y = 39 is halfway between the skybridge and partner with 6" on either side
-             .splineTo(Pose2d(28.0, -42.0, 180.0.toRadians), SplineInterpolator(180.0.toRadians, 180.0.toRadians))
-             .splineTo(Pose2d(48.0, -33.0, -90.0.toRadians), SplineInterpolator(180.0.toRadians, -90.0.toRadians))
-             .lineTo(Vector2d(48.0, -26.0), ConstantInterpolator(-90.0.toRadians))*/
+            // mid stone
+            .lineTo(Vector2d(-22.0,22.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+            .lineTo(Vector2d(-24.0, 22.0))
+            .strafeTo(Vector2d(-16.0, 38.0))
+            .reverse()
+            .lineTo(Vector2d(24.0, 38.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+            .reverse()
+            .lineTo(Vector2d(-24.0, 38.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+            .strafeTo(Vector2d(-46.0, 22.0))
+            .lineTo(Vector2d(-48.0, 22.0))
+            .strafeTo(Vector2d(-24.0, 38.0))
+            .reverse()
+            .lineTo(Vector2d(24.0, 38.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+            .reverse()
+            .lineTo(Vector2d(0.0, 38.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+        //left stone
+        /*.lineTo(Vector2d(-18.0, 30.0), LinearInterpolator(-90.0.toRadians, -45.0.toRadians))
+        .lineTo(Vector2d(-24.0, 24.00))
+        .strafeTo(Vector2d(-16.0, 38.0))
+        .reverse()
+        .lineTo(Vector2d(24.0, 38.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+        .reverse()
+        .lineTo(Vector2d(-38.0, 38.0), LinearInterpolator(0.0, -180.0.toRadians))
+        .strafeTo(Vector2d(-38.0, 22.0))
+        .lineTo(Vector2d(-42.0, 22.0))
+        .strafeTo(Vector2d(-16.0, 38.0))
+        .reverse()
+        .lineTo(Vector2d(24.0, 38.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+        .reverse()
+        .lineTo(Vector2d(0.0, 38.0), LinearInterpolator(0.0, -180.0.toRadians))*/
+//right stone
+        /*.lineTo(Vector2d(-30.0,22.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+        .lineTo(Vector2d(-32.0, 22.0))
+        .strafeTo(Vector2d(-16.0, 38.0))
+        .reverse()
+        .lineTo(Vector2d(24.0, 38.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+        .reverse()
+        .lineTo(Vector2d(-24.0, 38.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+        .strafeTo(Vector2d(-55.0, 22.0))
+        .lineTo(Vector2d(-60.0, 22.0))
+        .strafeTo(Vector2d(-24.0, 38.0))
+        .reverse()
+        .lineTo(Vector2d(24.0, 38.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))
+        .reverse()
+        .lineTo(Vector2d(0.0, 38.0), LinearInterpolator(-90.0.toRadians, -90.0.toRadians))*/
 
 
         list.add(builder1.build())
